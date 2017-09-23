8a5486f4b9c11645b1e528a843ff1033cfe334e4
fdeitylink <brianchristian16@gmail.com>
2017-09-23 08:17:56 -0400

    Made Type enums in Operation.kt implement OperationType

---
+++ kbagel/src/main/kotlin/io/fdeitylink/kbagel/Operation.kt
@@ -0,0 +1,32 @@
+package io.fdeitylink.kbagel
+
+internal interface OperationType<E> where E : OperationType<E>, E : Enum<E>
+
+internal sealed class Operation<E> where E : OperationType<E>, E : Enum<E> {
+    abstract val op : E
+}
+
+internal data class UnaryOperation(
+    override val op: UnaryOperation.Type
+) : Operation<UnaryOperation.Type>() {
+    enum class Type : OperationType<UnaryOperation.Type> {
+        NEGATE,  NOT
+    }
+}
+
+internal data class BinaryOperation(
+    override val op: BinaryOperation.Type
+) : Operation<BinaryOperation.Type>() {
+    enum class Type : OperationType<BinaryOperation.Type> {
+        MINUS, PLUS,
+        DIVIDE, MULTIPLY,
+
+        ASSIGN,
+
+        CHECK_EQUAL, CHECK_NOT_EQUAL,
+        CHECK_GREATER, CHECK_GREATER_EQUAL,
+        CHECK_LESS, CHECK_LESS_EQUAL,
+
+        AND, OR
+    }
+}
