b60495613951892b23dfb4db7627e5a2ae9ca152
fdeitylink <brianchristian16@gmail.com>
2017-09-24 23:12:39 -0400

    Added AstPrinter.kt (was removed before commit for Part II - 5 (Representing Code))

---
+++ kbagel/src/main/kotlin/io/fdeitylink/kbagel/AstPrinter.kt
@@ -0,0 +1,33 @@
+package io.fdeitylink.kbagel
+
+internal class AstPrinter : Expr.Visitor<String>() {
+    fun print(expr: Expr) = expr.accept(this)
+
+    override fun visit(u: Expr.Unary) = parenthesize(u.op.tokenType.char.toString(), u.operand)
+
+    override fun visit(b: Expr.Binary): String {
+        val tokenType = b.op.tokenType
+        val lexeme = when (tokenType) {
+            is SingleCharToken.Type -> tokenType.char.toString()
+            is MultiCharToken.Type -> tokenType.chars
+            is KeywordToken.Type -> tokenType.chars
+            else -> throw Error()
+        }
+
+        return parenthesize(lexeme, b.lOperand, b.rOperand)
+    }
+
+    override fun visit(l: Expr.Literal) = if (null == l.value) "nil" else l.value.toString()
+
+    override fun visit(g: Expr.Grouping) = parenthesize("group", g.expr)
+
+    fun parenthesize(name: String, vararg exprs: Expr): String {
+        val builder = StringBuilder()
+
+        builder.append('(').append(name)
+        exprs.forEach { builder.append(' ').append(it.accept(this)) }
+        builder.append(')')
+
+        return builder.toString()
+    }
+}
